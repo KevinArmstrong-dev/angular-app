cc530b41705ce8f375d72367ce07db2f99b5ead3
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2024-08-20 11:12:41 -0400

    Added the header component as in main component

---
+++ src/app/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header', //we use dash to avoid single words to avoid clashing with built in parts like using 'header'
+    // template: '', this is not advisable in general because it is only recommended for 3 liners or less  instead use ext file
+    templateUrl:'./header.component.html', 
+    standalone:true
+
+})
+
+export class HeaderComponent{}
