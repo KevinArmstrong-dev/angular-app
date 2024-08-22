59c7a2e50951048b7a50ea986693dea26572dc98
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2024-08-21 23:38:44 -0400

    Added some styles and removed redundant files

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -4,8 +4,8 @@ import { Component } from "@angular/core";
     selector: 'app-header', //we use dash to avoid single words to avoid clashing with built in parts like using 'header'
     // template: '', this is not advisable in general because it is only recommended for 3 liners or less  instead use ext file
     templateUrl:'./header.component.html', 
-    standalone:true
-
+    standalone:true,
+    styleUrls: ['./header.component.css',]
 })
 
 export class HeaderComponent{}
