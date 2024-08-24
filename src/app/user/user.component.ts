986af0122d6dd43dcc3101353c2378bcffd65e73
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2024-08-24 14:37:29 -0400

    added signals to the user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, signal, computed } from "@angular/core";
 import { DUMMY_USERS } from "../dummy-users";
 
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
 
 @Component({
     selector: 'app-user', //we use dash to avoid single words to avoid clashing with built in parts like using 'header'
@@ -13,5 +13,21 @@ const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 
 export class UserComponent{
-    selectedUser = DUMMY_USERS[randomIndex]; //all the properties here are available in the template
+     //all the properties here are available in the template
+    // selectedUser = DUMMY_USERS[randomIndex];
+    selectedUser = signal(DUMMY_USERS[randomIndex]); // Instatiating a signal
+    imagePath = computed(()=>'assets/users/' +this.selectedUser().avatar)
+    
+    
+    //This is a getter method so it is used as a property
+    // get imagePath(){
+    //     return 'assets/users/' +this.selectedUser().avatar; //getting data from a signal
+    // }
+
+    onSelectedUser(){
+        //global variables are overwritten and this will update with the state
+        const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length);
+        this.selectedUser.set(DUMMY_USERS[randomIndex]) //using signals for the properties
+        // this.selectedUser = DUMMY_USERS[randomIndex]; 
+    }
 }
