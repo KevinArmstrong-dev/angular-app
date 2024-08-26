3e951e4564b9654af98ce95b43d2b89f2fe77ce4
Kevin Armstrong Rwigamba <krwigamba@gmail.com>
2024-08-26 17:05:24 -0400

    added a required to match ts ! tag

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -13,8 +13,8 @@ import { DUMMY_USERS } from "../dummy-users";
 
 export class UserComponent{
      //all the properties here are available in the template
-    @Input()avatar!: string;
-    @Input()name!:string;
+    @Input({required:true})avatar!: string;
+    @Input({required:true})name!:string;
 
     get imagePath(){
         return 'assets/users/' + this.avatar;
